4d9e7beaba23c2e98cbcebf68fda0271a7e54b57
csgs104 <stefano98casagrande@gmail.com>
2023-04-21 15:37:46 +0200

    changes

--- Ex3/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ex3;
-
-public static class Constants
-{
-    /* public const string ConnectionString = @"
-        Server=127.0.0.1; Database=Aeroporti; Integrated Security=true; TrustServerCertificate=True"; */
-    public static string ConnectionString = @"
-        Server=127.0.0.1,1433; Database=Master; User Id=SA; Password=r00t.R00T; TrustServerCertificate=True";
-}
