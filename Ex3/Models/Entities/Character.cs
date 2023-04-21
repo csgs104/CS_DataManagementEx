d54b9f6c582731b1d33336274599aef84620e430
csgs104 <stefano98casagrande@gmail.com>
2023-04-21 15:42:14 +0200

    commit

---
+++ Ex3/Models/Entities/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex3.Models.Entities;
+
+public class Character
+{
+    // fileds
+
+    public int Id_Character { get; set; } // = null!;
+
+    public string Name { get; set; } = null!;
+
+
+    // relationships outiside
+
+    public virtual List<Artwork> Artworks { get; set; } = new();
+}
---
+++ Ex3/Repositories/DbContexts/Configurations/CharacterConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Ex3.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ex3.Repositories.DbContexts.Configurations;
+
+public class CharacterConfiguration : IEntityTypeConfiguration<Character>
+{
+    public void Configure(EntityTypeBuilder<Character> entity)
+    {
+        entity.HasKey(e => e.Id_Character).HasName("PK_Character");
+
+        entity.ToTable("Character");
+
+        entity.Property(e => e.Id_Character)
+            .HasColumnName("Id_Character")
+            .IsRequired(true);
+
+        entity.Property(e => e.Name)
+            .HasColumnName("Name")
+            .HasMaxLength(100)
+            .IsUnicode(true)
+            .IsRequired(true);
+    }
+}
