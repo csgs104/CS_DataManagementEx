a2430c17e91ca5308eb0687b01d28b0edb8bfa0c
csgs104 <stefano98casagrande@gmail.com>
2023-06-26 12:51:20 +0200

    minor changes

--- Ex2/Constants.cs
+++ Ex2/Constants.cs
@@ -5,5 +5,5 @@ public static class Constants
     /* public const string ConnectionString = @"
         Server=127.0.0.1; Database=Master; Integrated Security=true; TrustServerCertificate=True"; */
     public static string ConnectionString = @"
-        Server=127.0.0.1,1433; Database=Master; User Id=SA; Password=r00t.R00T; TrustServerCertificate=True";
+        Server=127.0.0.1,1433; Database=Master; User Id=SA; Password=R00t.r00T; TrustServerCertificate=True";
 }
